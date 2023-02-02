8e44ee75dee92ea03b2d3b258e7701ebc8b8a9b4
cchrissmm <chris@escapeecampers.com>
2023-02-02 17:35:16 +1100

    dia read working, config read not working

--- src/main.cpp
+++ src/main.cpp
@@ -12,58 +12,28 @@ SPIClass vspi;
 #define HSPI_SCK 14
 
 #define READ_DIA 0b00000000
+#define READ_CONTROL 0b01100000
 #define READ_REV 0b00100000
 
 byte spiCommand(SPIClass &spi, byte data, int CS)
 {
     spi.beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
-    digitalWrite(CS, LOW); 
+    digitalWrite(CS, LOW);
     byte spiResponse = spi.transfer(data);
     digitalWrite(CS, HIGH);
     spi.endTransaction();
     return spiResponse;
 }
 
-void serialRX()
+void readDiag()
 {
-    if (Serial.available())
-    {
-        String str = Serial.readStringUntil('\n');
 
-        if (str.startsWith("READDIA"))
-        {
-            byte response = spiCommand(hspi, READ_DIA, HSPI_CS);
-            Serial.print("spi response: ");
-            Serial.println(response, BIN);
-        }
-        if (str.startsWith("READREV"))
-        {
-            byte response = spiCommand(hspi, READ_REV, HSPI_CS);
-            Serial.print("spi response: ");
-            Serial.println(response, BIN);
-        }
-    }
-}
-
-
-
-void setup()
-{
-    Serial.begin(115200);
-    hspi.begin(HSPI_SCK, HSPI_MISO, HSPI_MOSI, HSPI_CS);
-    //hspi.begin();
-    pinMode(HSPI_CS, OUTPUT);
-}
-
-void loop()
-{
-    serialRX();
     byte response = spiCommand(hspi, READ_DIA, HSPI_CS);
     Serial.print("spi sent: ");
     Serial.println(READ_DIA, BIN);
 
-    int responseArray[8]; // Array to store each bit of the binary number
-    int index = 0;       // Index to keep track of the position in the array
+    int responseArray[8] = {0}; // Array to store each bit of the binary number
+    int index = 0;        // Index to keep track of the position in the array
 
     for (int i = 7; i >= 0; i--)
     {
@@ -87,5 +57,92 @@ void loop()
     Serial.print(" DIA1: " + String(responseArray[7]));
 
     Serial.println();
+}
+
+void readConfig()
+{
+    byte response = spiCommand(hspi, READ_CONTROL, HSPI_CS);
+    Serial.print("spi sent: ");
+    Serial.println(READ_CONTROL, BIN);
+
+    int responseArray[8] = {0}; // Array to store each bit of the binary number
+    int index = 0;        // Index to keep track of the position in the array
+
+    for (int i = 7; i >= 0; i--)
+    {
+        if ((response >> i) & 1)
+        {
+            responseArray[index++] = 1;
+        }
+        else
+        {
+            responseArray[index++] = 0;
+        }
+    }
+
+    Serial.print("CMD: " + String(responseArray[0]) + String(responseArray[1]) + String(responseArray[2]));
+    Serial.print(" OLDIS: " + String(responseArray[3]));
+    Serial.print(" SIN: " + String(responseArray[4]));
+    Serial.print(" SEN: " + String(responseArray[5]));
+    Serial.print(" SDIR: " + String(responseArray[6]));
+    Serial.print(" SPWM: " + String(responseArray[7]));
+    Serial.println();
+}
+
+void readRevision()
+{
+    byte response = spiCommand(hspi, READ_REV, HSPI_CS);
+    Serial.print("spi sent: ");
+    Serial.println(READ_REV, BIN);
+
+    int responseArray[8] = {0}; // Array to store each bit of the binary number
+    int index = 0;        // Index to keep track of the position in the array
+
+    for (int i = 7; i >= 0; i--)
+    {
+        if ((response >> i) & 1)
+        {
+            responseArray[index++] = 1;
+        }
+        else
+        {
+            responseArray[index++] = 0;
+        }
+    }
+
+    Serial.print("ZERO: " + String(responseArray[0]));
+    Serial.print(" ZERO: " + String(responseArray[1]));
+    Serial.print(" ONE: " + String(responseArray[2]));
+    Serial.print(" ZERO: " + String(responseArray[3]));
+    Serial.print(" REV: " + String(responseArray[4]) + String(responseArray[5]) + String(responseArray[6]) + String(responseArray[7]));
+    Serial.println();
+}
+
+void serialRX()
+{
+    if (Serial.available())
+    {
+        String str = Serial.readStringUntil('\n');
+
+        if (str.startsWith("READDIA"))
+        {
+            readDiag();
+           // readConfig();
+           // readRevision();
+        }
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    hspi.begin(HSPI_SCK, HSPI_MISO, HSPI_MOSI, HSPI_CS);
+    // hspi.begin();
+    pinMode(HSPI_CS, OUTPUT);
+}
+
+void loop()
+{
+    serialRX();
     delay(100);
 }
